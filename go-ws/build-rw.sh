52c95f1b1028e38a6a745582acf1234290b07517
Sudharsan R <sud.rang@gmail.com>
2018-01-22 11:41:30 -0800

    Minor fixes

--- go-ws/build-rw.sh
+++ go-ws/build-rw.sh
@@ -9,14 +9,14 @@ sudo mkdir -p ./bin/reader
 sudo mkdir -p ./bin/writer
 
 echo "Building Reader....."
-sudo docker run  -v `pwd`:/gows/ -w /gows/src/reader -e "GOBIN=/gows/bin/reader/" -e "GOPATH=/gows" -it gobs /bin/sh -c "CGO_ENABLED=0 GOOS=linux go install -a -ldflags '-extldflags "-static"' ."
+sudo docker run  -v `pwd`:/gows/ -w /gows/src/reader -e "GOBIN=/gows/bin/reader/" -e "GOPATH=/gows" -it gobs /bin/sh -c "go get; CGO_ENABLED=0 GOOS=linux go install -a -ldflags '-extldflags "-static"' ."
 if [ ! $? -eq 0 ]; then
     echo "Failed to build reader $?"
 fi
 sudo cp src/reader/*.html ./bin/reader
 
 echo "Building Writer....."
-sudo docker run -v `pwd`:/gows/ -w /gows/src/writer -e "GOBIN=/gows/bin/writer/" -e "GOPATH=/gows" -it gobs /bin/sh -c "CGO_ENABLED=0 GOOS=linux go install -a -ldflags '-extldflags "-static"' ."
+sudo docker run -v `pwd`:/gows/ -w /gows/src/writer -e "GOBIN=/gows/bin/writer/" -e "GOPATH=/gows" -it gobs /bin/sh -c "go get; CGO_ENABLED=0 GOOS=linux go install -a -ldflags '-extldflags "-static"' ."
 if [ ! $? -eq 0 ]; then
     echo "Failed to build writer $?"
 fi
