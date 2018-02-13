fd21d7a86ea7253e2af1af695782568ccec5cf27
Sudharsan R <sud.rang@gmail.com>
2018-02-12 21:17:18 -0800

    Add redis monitoring

--- deploy/kubernetes/deploy-all.sh
+++ deploy/kubernetes/deploy-all.sh
@@ -1,6 +1,5 @@
 kubectl create -f deploy-zookeeper.yaml
-kubectl create -f deploy-redis-pod.yaml
-kubectl create -f deploy-redis-service.yaml
+kubectl create -f deploy-redis.yaml
 kubectl create config mysql-config --from-file=schema.sql:schema.sql
 kubectl create -f deploy-mysql-pod.yaml
 kubectl create -f deploy-mysql-service.yaml
@@ -8,3 +7,4 @@ kubectl create -f deploy-reader.yaml
 kubectl create -f deploy-writer.yaml
 kubectl create -f deploy-ingress.yaml
 kubectl create -f deploy-prometheus.yaml
+kubectl create -f deploy-grafana.yaml
